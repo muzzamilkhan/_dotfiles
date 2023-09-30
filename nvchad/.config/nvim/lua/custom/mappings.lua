5ccf16bd497e39ac0f74fb4497784aa18985e777
Muzzamil Khan <muzzamil.khan@author-it.com>
2023-09-30 15:00:00 +1300

    Nvim updates

--- nvchad/.config/nvim/lua/custom/mappings.lua
+++ nvchad/.config/nvim/lua/custom/mappings.lua
@@ -18,12 +18,23 @@ M.general = {
 	n = {
 		["L"] = { ":bnext<CR>" },
 		["H"] = { ":bprevious<CR>" },
+		["<leader>Q"] = { ':%bdelete|edit #|normal `"<CR>', "Close all other buffers" },
 	},
 }
 M.nvimtree = {
 	n = {
-		["E"] = { "<cmd> NvimTreeFocus <CR>", "focus nvimtree" },
-		["<leader>e"] = { "<cmd> NvimTreeToggle <CR>", "toggle nvimtree" },
+		["E"] = { "<cmd> NvimTreeFocus <CR>", "Focus nvimtree" },
+		["<leader>e"] = { "<cmd> NvimTreeToggle <CR>", "Toggle nvimtree" },
+		["<leader>gr"] = {
+			function()
+				local node = require("nvim-tree.lib").get_node_at_cursor()
+				if not node then
+					return
+				end
+				require("telescope.builtin").live_grep({ search_dirs = { node.absolute_path .. "/*" } })
+			end,
+			"Grep in current dir",
+		},
 	},
 }
 

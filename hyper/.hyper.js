e61ea5f62cb0dec84fec8bad088b7c81c22642dd
Muzzamil Khan <muzzamil.khan@author-it.com>
2024-01-24 00:59:54 +1300

    Add hyper config

---
+++ hyper/.hyper.js
@@ -0,0 +1,16 @@
+module.exports = {
+    plugins: ['hypurr#latest'],
+    config: {
+        // choose either `'stable'` for receiving highly polished,
+        catppuccinTheme: 'Mocha',
+        // or `'canary'` for less polished but more frequent updates
+        updateChannel: 'stable',
+
+        // default font size in pixels for all tabs
+        fontSize: 20,
+
+        // font family with optional fallbacks
+        fontFamily:
+            'JetBrainsMono Nerd Font Mono',
+    }
+}

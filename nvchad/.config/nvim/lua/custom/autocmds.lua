5120af810379b92ce9ff3bc4c2a226caf4afe680
Muzzamil Khan <muzzamil.khan@author-it.com>
2024-06-14 17:07:15 +1200

    tweaks

--- nvchad/.config/nvim/lua/custom/autocmds.lua
+++ nvchad/.config/nvim/lua/custom/autocmds.lua
@@ -1,3 +1,18 @@
+local function open_nvim_tree(data)
+	-- buffer is a directory
+	local directory = vim.fn.isdirectory(data.file) == 1
+
+	if not directory then
+		return
+	end
+
+	-- change to the directory
+	vim.cmd.cd(data.file)
+
+	-- open the tree
+	require("nvim-tree.api").tree.open()
+end
+
 vim.cmd([[
   augroup _general_settings
     autocmd!
@@ -24,22 +39,13 @@ vim.cmd([[
     autocmd VimResized * tabdo wincmd = 
   augroup end
 
+  augroup _open_readme
+    autocmd!
+    autocmd VimEnter * silent! edit README.md
+  augroup end
+
+
 ]])
-
-local function open_nvim_tree(data)
-	-- buffer is a directory
-	local directory = vim.fn.isdirectory(data.file) == 1
-
-	if not directory then
-		return
-	end
-
-	-- change to the directory
-	vim.cmd.cd(data.file)
-
-	-- open the tree
-	require("nvim-tree.api").tree.open()
-end
 
 vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
 
--- typecraft-nvim/.config/nvim/lua/plugins/none-ls.lua
+++ typecraft-nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -6,6 +6,8 @@ return {
 			sources = {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.prettier,
+
+				null_ls.builtins.diagnostics.eslint_d,
 			},
 		})
 
